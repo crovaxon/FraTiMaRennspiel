7f1700f9c90bd3fa6363f8b73c9052943dcfa1f3
Marek Maciejewski <keram0maruku@hotmail.com>
2017-05-16 10:50:00 +0200

    Neue Version vom Klassendiagramm. Automatisch generierte Klassen vom Diagramm erstellt.

---
+++ FraTiMaRennspiel/src/rennspiel/Auto.java
@@ -0,0 +1,27 @@
+package rennspiel;
+
+public class Auto {
+
+    private float geschwindigkeit;
+
+    private float tank;
+
+    private float verbrauch;
+
+    public void beschleunigen(Wetter pAktuellesWetter) {
+    }
+
+    public void bremsen(Wetter pAktuellesWetter) {
+    }
+
+    public void weiter(Wetter pAktuellesWetter) {
+    }
+
+    public float getGeschwindigkeit() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public float getTank() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ FraTiMaRennspiel/src/rennspiel/Spieler.java
@@ -0,0 +1,15 @@
+package rennspiel;
+
+public class Spieler {
+
+    private String name;
+
+    private Auto auto;
+
+    public void SetName() {
+    }
+
+    public String GetName() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
